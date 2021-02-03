e38bd8e88dede5be136cbd37b9c92cfa6f4f3b94
GitHub Action <action@github.com>
2021-02-03 11:41:33 +0000

    Rating Service commit hash: 5658ec3

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Rating Service commit hash: 92a4143"
+    release_note="Rating Service commit hash: 5658ec3"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
